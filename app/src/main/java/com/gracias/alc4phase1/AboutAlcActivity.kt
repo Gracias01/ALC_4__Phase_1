f117d0523a008818ebeef87c322b2b11341d6401
Gracias <graciasdelaflore@gmail.com>
2019-07-15 00:01:36 +0100

    webview commit

--- app/src/main/java/com/gracias/alc4phase1/AboutAlcActivity.kt
+++ app/src/main/java/com/gracias/alc4phase1/AboutAlcActivity.kt
@@ -3,14 +3,15 @@ package com.gracias.alc4phase1
 import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import kotlinx.android.synthetic.main.activity_about_alc.*
-import android.net.NetworkInfo
-import android.content.Context.CONNECTIVITY_SERVICE
-import android.support.v4.content.ContextCompat.getSystemService
 import android.net.ConnectivityManager
+import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Toast
+import android.net.http.SslError
+import android.webkit.SslErrorHandler
+
+
 
 
 class AboutAlcActivity : AppCompatActivity() {
@@ -21,17 +22,28 @@ class AboutAlcActivity : AppCompatActivity() {
 
         val actionBar = supportActionBar
         val title = getString(R.string.about_alc);
+        val url = getString(R.string.url);
         actionBar!!.title =title
 
         actionBar.setDisplayHomeAsUpEnabled(true)
+
+        webview.apply {
+            settings.javaScriptEnabled = true
+            settings.loadWithOverviewMode = true
+            settings.useWideViewPort = true
+            webViewClient = object : WebViewClient() {
+                override fun onReceivedSslError(view: WebView, handler: SslErrorHandler, error: SslError) {
+                    handler.proceed()
+                }
+            }
+
+        }
 
         if (!isNetworkAvailable()) {
             Toast.makeText(this, "Please check your internet connection", Toast.LENGTH_SHORT).show()
 
         } else {
-            webview.webViewClient = WebViewClient()
-            webview.loadUrl("https://andela.com/alc/")
-
+            webview.loadUrl(url)
 
         }
 
@@ -40,7 +52,7 @@ class AboutAlcActivity : AppCompatActivity() {
     private fun isNetworkAvailable(): Boolean {
         val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         val activeNetworkInfo = connectivityManager.activeNetworkInfo
-        return activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected
     }
 
     override fun onSupportNavigateUp(): Boolean {
